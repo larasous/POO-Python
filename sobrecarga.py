c9172862f86caa9304fd12357840c3bee9ac8bb0
larasous <lara.amanny@alu.ufc.br>
2021-12-28 18:03:21 -0300

    sobrecarga de operadores

---
+++ sobrecarga.py
@@ -0,0 +1,51 @@
+"""
+No Python, o comportamento dos operadores é definido por métodos especiais.
+Vamos alterar o comportamento de operadores com classes definidas pelo usuário
+"""
+class Retangulo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def get_area(self):
+        return self.x * self.y
+    
+    def __repr__(self):
+        return f"<class 'Retangulo{self.x, self.y}'>"
+    
+    def __add__(self, other):  # adição
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Retangulo(novo_x, novo_y)
+    
+    def __lt__(self, other):  # menor que
+        a1 = self.get_area()
+        a2 = self.get_area()
+        
+        if a1 < a2: return True
+        else: return False
+        
+    def __gt__(self, other):  # maior que
+        a1 = self.get_area()
+        a2 = other.get_area()
+        
+        if a1 > a2: return True
+        else: return False
+        
+    def __eq__(self, other):  # igualdade
+        a1 = self.get_area()
+        a2 = other.get_area()
+        
+        if a1 == a2: return True
+        else: return False
+
+r1 = Retangulo(10, 20)
+r2 = Retangulo(10, 20)
+r3 = r1 + r2
+print(r3)
+print(r3 < r1)
+print(r3 > r1)
+print(r1 == r2)
+print(r1 == r3)
+        
+        
